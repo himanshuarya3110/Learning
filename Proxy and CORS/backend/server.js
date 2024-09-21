76f54350cefc280beebc9a47a3849e5c86cc376f
himanshuarya3110 <himanshuarya3110@gmail.com>
2024-09-22 03:16:08 +0530

    change type to module to work.

--- Proxy and CORS/backend/server.js
+++ Proxy and CORS/backend/server.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
     res.send('Server is Running!!')
 })
 app.listen(port, () => {
-    console.log(`Server at http://localhost:${port}!`)
+    console.log(`Server at http://localhost:${port}`)
 })
