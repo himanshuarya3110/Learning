81d03d043331c193ed351fb2be4ad98bff8505d9
himanshuarya3110 <himanshuarya3110@gmail.com>
2024-09-22 02:44:42 +0530

    Learn Redux toolkit

---
+++ reduxproject/src/App.jsx
@@ -0,0 +1,15 @@
+import AddTodo from './Components/AddTodo';
+import Todos from './Components/Todos';
+
+function App() {
+
+  return (
+    <>
+      <h1>learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
