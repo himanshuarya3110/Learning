3a12c8bd03c7f3f63c0e7a5925581ab4a4c6cabc
Himanshu Arya <85223926+himanshuarya3110@users.noreply.github.com>
2024-11-19 16:16:53 +0530

    Time: 0 ms (100%), Space: 8.4 MB (15.84%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        // int c = n;
+        again:
+        long long int sum = 0;
+        while(num > 0) {
+            long long int rem = num % 10;
+            sum += rem;
+            num /= 10;
+        }
+        if(sum < 10) return sum;
+        else {
+            num = sum;
+            goto again;
+        }
+        return sum;
+    }
+};
